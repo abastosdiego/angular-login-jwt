515f5cbec7c7f00da6fb23128ffb7dccbf9c3847
Diego <abastosdiego@gmail.com>
2023-09-25 18:45:31 -0300

    Ajustes area publica

--- src/app/modules/area-publica/area-publica.module.ts
+++ src/app/modules/area-publica/area-publica.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AreaPublicaRoutingModule } from './area-publica-routing.module';
-import { IndexComponent } from './components/index/index.component';
+import { IndexComponent } from './index/index.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TokenInterceptor } from 'src/app/interceptors/token.interceptor';
 
